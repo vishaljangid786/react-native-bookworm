bcd33e770f8337755bfd4e253eff0aa5719097b5
Vishal Jangid <vishaljangid80550786@gmail.com>
2025-06-18 20:55:54 +0530

    com

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// job.start();
+job.start();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));  // ⬅️ Increase limit here
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));  // If you're using form data
